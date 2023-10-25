89e4523b429f67f231873155b424062d30ea2ce8
Fer Aguirre <77854667+fer-aguirre@users.noreply.github.com>
2023-10-24 23:08:54 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Elecciones Argentina 2023',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Análisis de datos de ataques contra periodistas en X durante las elecciones en Argentina en 2023',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
